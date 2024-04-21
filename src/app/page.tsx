c2752aac20c94539e180527604194a6194b8fb21
fzl96 <fpsdragon333@gmail.com>
2024-04-21 16:47:15 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="">
+      <div>Wisethetic Lab</div>
+    </main>
+  );
+}
