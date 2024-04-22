67ef0b4e777f0bdf350e38715874e5d38e8b768a
fzl96 <fpsdragon333@gmail.com>
2024-04-23 00:25:19 +0700

    feat: add metadata

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "Wisethetic Lab",
+  description:
+    "Your dedicated creative partner in shaping an exceptional online presence.",
+  url: "https://wisetheticlab.store",
+  ogImage: "https://wisethetic.store/og.jpg",
+  links: {
+    twitter: "https://twitter.com/MrWisethetic",
+    instagram: "https://www.instagram.com/mr.wisethetic",
+  },
+};
