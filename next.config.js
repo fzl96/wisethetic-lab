b0d9e3ea05e2899e8de17e7010c61d66b2dd0036
fzl96 <fpsdragon333@gmail.com>
2024-04-23 06:52:49 +0700

    build: allow uploadthing for image opt

--- next.config.js
+++ next.config.js
@@ -16,6 +16,12 @@ const config = {
         hostname: "*.googleusercontent.com",
         port: "",
         pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "**",
       }
     ]
   }
