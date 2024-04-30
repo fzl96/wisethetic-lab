f10e2dea2e0c6d0f28ced29415806cca240140e6
fzl96 <fpsdragon333@gmail.com>
2024-05-01 04:55:25 +0700

    feat: add cards for package details with loading skeleton

---
+++ src/app/(dashboard)/dashboard/packages/[name]/[packageName]/loading.tsx
@@ -0,0 +1,42 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function CategoryLoading() {
+  return (
+    <Card className="overflow-hidden">
+      <CardHeader className="flex flex-row items-start">
+        <div className="grid gap-2">
+          <CardTitle className="group flex items-center gap-2 text-lg">
+            <Skeleton className="h-6 w-20" />
+          </CardTitle>
+          <CardDescription>
+            <Skeleton className="h-4 w-40" />
+          </CardDescription>
+        </div>
+        <div className="ml-auto flex items-center gap-1">
+          <Skeleton className="h-8 w-8" />
+        </div>
+      </CardHeader>
+      <Separator />
+      <CardContent className="p-6 text-sm dark:bg-card">
+        <div className="grid h-80 place-items-center overflow-hidden rounded-lg">
+          <Skeleton className="h-full w-full" />
+        </div>
+      </CardContent>
+      <Separator />
+      <CardFooter className="flex flex-row items-center px-6 py-3">
+        <div className="text-xs text-muted-foreground">
+          <Skeleton className="h-5 w-20" />
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/(dashboard)/dashboard/packages/[name]/[packageName]/page.tsx
+++ src/app/(dashboard)/dashboard/packages/[name]/[packageName]/page.tsx
@@ -1,6 +1,18 @@
 import { getPackageByNameWithProducts } from "@/server/api/packages/queries";
 import { reverseSlug } from "@/lib/utils";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+import { format } from "date-fns";
+
 export default async function PackageNamePage({
   params,
 }: {
@@ -12,6 +24,40 @@ export default async function PackageNamePage({
   const categoryName = reverseSlug(params.name);
   const pkgName = reverseSlug(params.packageName);
   const pkg = await getPackageByNameWithProducts(categoryName, pkgName);
-  console.log(pkg);
-  return <div>{JSON.stringify(params, null, 2)}</div>;
+  return (
+    <Card className="overflow-hidden">
+      <CardHeader className="flex flex-row items-start gap-5">
+        <div className="grid gap-0.5">
+          <CardTitle className="group flex items-center gap-2 text-lg">
+            {pkg?.name}
+          </CardTitle>
+          <CardDescription>{pkg?.description}</CardDescription>
+        </div>
+        <div className="ml-auto flex items-center gap-1">
+          {/* {category && <CategoryMenu category={category} />} */}
+        </div>
+      </CardHeader>
+      <Separator />
+      <CardContent className="p-6 text-sm dark:bg-card">
+        <div className="h-80 overflow-hidden rounded-lg">
+          <Image
+            src={pkg?.image ?? "/placeholder.jpg"}
+            alt={pkg?.name ?? "Category Image"}
+            width={300}
+            height={200}
+            className="h-full w-full object-cover transition-all duration-500 hover:scale-105 dark:brightness-[0.5] dark:grayscale hover:dark:grayscale-0"
+          />
+        </div>
+      </CardContent>
+      <Separator />
+      <CardFooter className="flex flex-row items-center px-6 py-3">
+        <div className="text-xs text-muted-foreground">
+          Updated{" "}
+          <time dateTime={format(new Date(pkg?.updatedAt ?? ""), "yyyy-MM-dd")}>
+            {format(new Date(pkg?.updatedAt ?? ""), "MMMM d, yyyy")}
+          </time>
+        </div>
+      </CardFooter>
+    </Card>
+  );
 }
