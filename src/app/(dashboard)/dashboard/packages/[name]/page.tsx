f3ebaf27c6f9d7f032d8e1882ad3a2dcdbd5982e
fzl96 <fpsdragon333@gmail.com>
2024-05-01 04:55:07 +0700

    refactor: remove unused vars

--- src/app/(dashboard)/dashboard/packages/[name]/page.tsx
+++ src/app/(dashboard)/dashboard/packages/[name]/page.tsx
@@ -1,11 +1,8 @@
-import { getPackagesWithCategory } from "@/server/api/packages/queries";
-
 export default async function CategoryName({
   params,
 }: {
   params: { name: string };
 }) {
-  const packages = await getPackagesWithCategory();
   return (
     <div>
       <h1>{params.name}</h1>
