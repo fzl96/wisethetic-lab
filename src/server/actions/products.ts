abd56d94fe9d74a0fe5381a6df4ded5d87c118de
fzl96 <fpsdragon333@gmail.com>
2024-05-01 15:57:10 +0700

    feat: add server actions for product

---
+++ src/server/actions/products.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "@/server/api/products/mutations";
+import {
+  type ProductId,
+  type NewProductParams,
+  type UpdateProductParams,
+  productIdSchema,
+  insertProductParams,
+  updateProductParams,
+} from "@/server/db/schema/product";
+
+const handleErrors = (e: unknown) => {
+  const errMsg = { error: "An error occurred" };
+  if (e instanceof Error) {
+    const err = { error: e.message };
+    return e.message.length > 0 ? err : errMsg;
+  }
+  if (e && typeof e === "object" && "error" in e) {
+    const errAsStr = e.error as string;
+    return errAsStr.length > 0 ? { error: errAsStr } : errMsg;
+  }
+  return errMsg;
+};
+
+const revalidateProduct = () => revalidatePath("/dashboard/packages");
+
+export const createProductAction = async (input: NewProductParams) => {
+  try {
+    const payload = insertProductParams.parse(input);
+    const res = await createProduct(payload);
+    if (res.error) throw new Error(res.error);
+    revalidateProduct();
+    return { message: "Product created successfully" };
+  } catch (e) {
+    return handleErrors(e);
+  }
+};
+
+export const updateProductAction = async (input: UpdateProductParams) => {
+  try {
+    const payload = updateProductParams.parse(input);
+    const res = await updateProduct(payload.id, payload);
+
+    if (res.error) throw new Error(res.error);
+
+    revalidateProduct();
+    return { message: "Product updated successfully" };
+  } catch (e) {
+    return handleErrors(e);
+  }
+};
+
+export const deleteProductAction = async (productId: ProductId) => {
+  try {
+    await deleteProduct(productId);
+    revalidateProduct();
+  } catch (e) {
+    return handleErrors(e);
+  }
+};
