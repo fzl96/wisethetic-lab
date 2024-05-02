2228a1e3a40a327d39a54426d7473b4bcab42605
fzl96 <fpsdragon333@gmail.com>
2024-05-02 18:37:19 +0700

    refactor: move link outside card comp

--- src/app/(homepage)/_components/category-card.tsx
+++ src/app/(homepage)/_components/category-card.tsx
@@ -1,27 +1,24 @@
 import { type Category } from "@/server/db/schema/product";
 import Image from "next/image";
-import Link from "next/link";
 
 export function CategoryCard({ category }: { category: Category }) {
   return (
-    <Link href="#" className="">
-      <div className="bg-home-card-background hover:bg-home-card-background-hover border-home-border rounded-xl border p-3 shadow-sm transition-all duration-200">
-        <div className="h-96 overflow-hidden rounded-lg lg:h-[35rem]">
-          <Image
-            src={category.image ?? "/placeholder.jpg"}
-            alt={category.name}
-            width={300}
-            height={384}
-            className="h-full w-full object-cover transition-all duration-500 hover:scale-105"
-          />
-        </div>
-        <div className="space-y-2 px-2 py-5 text-left ">
-          <h3 className="text-left font-accent text-2xl font-medium">
-            {category.name}
-          </h3>
-          <p className="font-light">From IDR 35.000</p>
-        </div>
+    <div className="rounded-xl border border-home-border bg-home-card-background p-3 shadow-sm transition-all duration-200 hover:bg-home-card-background-hover">
+      <div className="h-96 overflow-hidden rounded-lg lg:h-[35rem]">
+        <Image
+          src={category.image ?? "/placeholder.jpg"}
+          alt={category.name}
+          width={300}
+          height={384}
+          className="h-full w-full object-cover transition-all duration-500 hover:scale-105"
+        />
       </div>
-    </Link>
+      <div className="space-y-2 px-2 py-5 text-left ">
+        <h3 className="text-left font-accent text-2xl font-medium">
+          {category.name}
+        </h3>
+        <p className="font-light">From IDR 35.000</p>
+      </div>
+    </div>
   );
 }
--- src/app/(homepage)/_components/category-section.tsx
+++ src/app/(homepage)/_components/category-section.tsx
@@ -1,14 +1,22 @@
+import Link from "next/link";
+
 import { getCategories } from "@/server/api/categories/queries";
 import { CategoryCard } from "./category-card";
+import { convertToSlug } from "@/lib/utils";
 
 export async function CategorySection() {
   const categories = await getCategories({});
 
   return (
     <div className="mt-40 grid w-full gap-5 lg:grid-cols-3 lg:gap-8">
-      {categories.map((category) => (
-        <CategoryCard category={category} key={category.id} />
-      ))}
+      {categories.map((category) => {
+        const link = convertToSlug(category.name);
+        return (
+          <Link href={`/products/${link}`} key={category.id}>
+            <CategoryCard category={category} key={category.id} />
+          </Link>
+        );
+      })}
     </div>
   );
 }
