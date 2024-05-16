b3b200f6142eb63f86f6f9e05dcb2287e1ebee0b
fzl96 <fpsdragon333@gmail.com>
2024-05-16 13:21:49 +0700

    refactor: remove fetch order

--- src/app/api/payment-notification/route.ts
+++ src/app/api/payment-notification/route.ts
@@ -14,76 +14,64 @@ export async function POST(req: NextRequest) {
     signature_key,
     gross_amount,
   } = await req.json();
+  const data = await req.json();
+  console.log(data);
 
   if (!order_id || typeof order_id !== "string")
     return NextResponse.json({ status: "error", message: "Invalid order_id" });
 
-  db.query.orders
-    .findFirst({
-      where: (orders, { eq }) => eq(orders.id, order_id),
-    })
-    .then(async (order) => {
-      if (!order)
-        return NextResponse.json({
-          status: "error",
-          message: "Order not found",
-        });
+  const hash = crypto
+    .createHash("sha512")
+    .update(
+      `${order_id}${status_code}${gross_amount}${process.env.MIDTRANS_SERVER_KEY}`,
+    )
+    .digest("hex");
 
-      const hash = crypto
-        .createHash("sha512")
-        .update(
-          `${order_id}${status_code}${gross_amount}${process.env.MIDTRANS_SERVER_KEY}`,
-        )
-        .digest("hex");
-
-      if (!hash !== signature_key)
-        return NextResponse.json({
-          status: "error",
-          message: "Invalid signature",
-        });
-
-      if (transaction_status == "capture") {
-        if (fraud_status == "accept") {
-          await db
-            .update(payments)
-            .set({ status: "completed", method: payment_type })
-            .where(eq(payments.orderId, order_id));
-          await db
-            .update(orders)
-            .set({ status: "process" })
-            .where(eq(orders.id, order_id));
-        }
-      } else if (transaction_status == "settlement") {
-        console.log("settled");
-        await db
-          .update(payments)
-          .set({ status: "completed", method: payment_type })
-          .where(eq(payments.orderId, order_id));
-        await db
-          .update(orders)
-          .set({ status: "process" })
-          .where(eq(orders.id, order_id));
-      } else if (
-        transaction_status == "cancel" ||
-        transaction_status == "deny" ||
-        transaction_status == "expire"
-      ) {
-        await db
-          .update(payments)
-          .set({ status: "cancelled" })
-          .where(eq(payments.orderId, order_id));
-        await db
-          .update(orders)
-          .set({ status: "cancelled" })
-          .where(eq(orders.id, order_id));
-      } else if (transaction_status == "pending") {
-        // TODO set transaction status on your database to 'pending' / waiting payment
-        // and response with 200 OK
-      }
-    })
-    .catch((err) => {
-      console.log(err);
+  if (!hash !== signature_key)
+    return NextResponse.json({
+      status: "error",
+      message: "Invalid signature",
     });
+
+  if (transaction_status == "capture") {
+    if (fraud_status == "accept") {
+      await db
+        .update(payments)
+        .set({ status: "completed", method: payment_type })
+        .where(eq(payments.orderId, order_id));
+      await db
+        .update(orders)
+        .set({ status: "process" })
+        .where(eq(orders.id, order_id));
+    }
+  } else if (transaction_status == "settlement") {
+    console.log("settled");
+    await db
+      .update(payments)
+      .set({ status: "completed", method: payment_type })
+      .where(eq(payments.orderId, order_id));
+    await db
+      .update(orders)
+      .set({ status: "process" })
+      .where(eq(orders.id, order_id));
+  } else if (
+    transaction_status == "cancel" ||
+    transaction_status == "deny" ||
+    transaction_status == "expire"
+  ) {
+    await db
+      .update(payments)
+      .set({ status: "cancelled" })
+      .where(eq(payments.orderId, order_id));
+    await db
+      .update(orders)
+      .set({ status: "cancelled" })
+      .where(eq(orders.id, order_id));
+  } else if (transaction_status == "pending") {
+    // TODO set transaction status on your database to 'pending' / waiting payment
+    // and response with 200 OK
+    console.log("pending");
+  }
 
   return NextResponse.json({
     status: "success",
