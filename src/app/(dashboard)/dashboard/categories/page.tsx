d1c9902efe28e2003a83b5845cef45617009b554
fzl96 <fpsdragon333@gmail.com>
2024-07-09 06:06:54 +0700

    feat: add filler image

--- src/app/(dashboard)/dashboard/categories/page.tsx
+++ src/app/(dashboard)/dashboard/categories/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export default async function CategoriesPage() {
   return (
-    <div>
-      <p>Test</p>
+    <div className="grid w-full place-items-center overflow-hidden">
+      <div className="w-[80%] overflow-hidden">
+        <Image
+          src="/Loading-Time.svg"
+          width={200}
+          height={200}
+          alt="placeholder"
+          className="h-full w-full object-cover"
+        />
+      </div>
+      <span className="text-lg">Select a Category</span>
     </div>
   );
 }
--- src/app/(dashboard)/dashboard/packages/[name]/page.tsx
+++ src/app/(dashboard)/dashboard/packages/[name]/page.tsx
@@ -1,11 +1,18 @@
-export default async function CategoryName({
-  params,
-}: {
-  params: { name: string };
-}) {
+import Image from "next/image";
+
+export default async function CategoryName() {
   return (
-    <div>
-      <h1>{params.name}</h1>
+    <div className="grid w-full place-items-center overflow-hidden">
+      <div className="w-[80%] overflow-hidden">
+        <Image
+          src="/Chill-Time.svg"
+          width={200}
+          height={200}
+          alt="placeholder"
+          className="h-full w-full object-cover"
+        />
+      </div>
+      <span className="text-lg">Select a Package</span>
     </div>
   );
 }
