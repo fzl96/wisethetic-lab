070e790fdc24239a11c48fd07aef16e9d5c5aaa4
fzl96 <fpsdragon333@gmail.com>
2024-07-09 08:41:52 +0700

    fix: forgot to add placeholder=blur

--- src/app/(homepage)/portfolio/_components/dynamic-image.tsx
+++ src/app/(homepage)/portfolio/_components/dynamic-image.tsx
@@ -3,12 +3,14 @@ import { getImage } from "@/lib/get-image";
 
 export async function DynamicImage({ url }: { url: string }) {
   const { base64, img } = await getImage(url);
+  console.log(base64);
 
   return (
     <div className="relative mb-4">
       <Image
         {...img}
         alt="portfolio image"
+        placeholder="blur"
         blurDataURL={base64}
         sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
       />
