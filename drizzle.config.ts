dfd9580aca01903b5429124f6cde2917b6c06d4e
fzl96 <fpsdragon333@gmail.com>
2024-09-17 21:43:03 +0700

    build: update drizzle-orm, kit, next-auth, adapter

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,8 @@ import { env } from "@/env";
 
 export default {
   schema: "./src/server/db/schema",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: env.POSTGRES_URL,
+    url: env.POSTGRES_URL,
   },
 } satisfies Config;
