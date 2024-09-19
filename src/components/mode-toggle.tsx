cb8cc33581877f9aaf3af1eb3ff09363a5284192
fzl96 <fpsdragon333@gmail.com>
2024-09-19 07:10:16 +0700

    feat: add props to style dropdown trigger
    add options to change the variant of the dropdown trigger button and add
    classname

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -11,14 +11,30 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 
-export function ModeToggle() {
+interface ModeToggleProps {
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+  className?: string;
+}
+
+export function ModeToggle({ variant, className }: ModeToggleProps) {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button
+          variant={variant ?? "outline"}
+          size="icon"
+          className={cn(className)}
+        >
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
