394b4ec40dd301d542f1de7cbe0e7d274369daa3
fzl96 <fpsdragon333@gmail.com>
2024-11-11 03:40:22 +0700

    fix: Remove old checkout page code

--- src/app/(homepage)/out/_components/form.tsx
+++
@@ -1,8 +0,0 @@
-import { getCart } from "@/server/api/carts/queries";
-import { CheckoutFormClient } from "./form-client";
-
-export async function CheckoutForm() {
-  const cart = await getCart();
-
-  return cart && <CheckoutFormClient cart={cart} />;
-}
